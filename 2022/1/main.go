3b571f538d774e92c7e662ff7edcd7e375a71be1
Lucas Clerisse <lucasclerisse@protonmail.com>
2022-12-01 20:11:57 +0800

    feat: commit first day solution

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -4,9 +4,86 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+
+	"golang.org/x/exp/constraints"
 )
+
+type Elf struct {
+	// Bag contains the calories of each food
+	Bag []int
+}
+
+func (e Elf) TotalCalories() int {
+	cal := 0
+	for _, c := range e.Bag {
+		cal += c
+	}
+	return cal
+}
+
+// DoForElf executes the given function for every elf in the given input.
+func DoForElf(tokens []string, f func(Elf, int)) error {
+	bag := []int{}
+	idx := 0
+
+	for _, token := range tokens {
+		if token == "" {
+			f(Elf{bag}, idx)
+			// reset bag
+			bag = []int{}
+			idx += 1
+		} else {
+			cal, err := strconv.Atoi(token)
+			if err != nil {
+				log.Fatalf("failed to parse calories: %s", err)
+			}
+			bag = append(bag, cal)
+		}
+	}
+
+	return nil
+}
+
+// Topk returns the top k elves with the highest total calories.
+func Topk(tokens []string, k int) ([]int, error) {
+	tops := make([]int, k)
+
+	compute := func(elf Elf, idx int) {
+		totalCalories := elf.TotalCalories()
+		index := sort.Search(k, func(index int) bool { return tops[index] <= totalCalories })
+
+		if index == k {
+			// sort.Search returns the index of last element if it could not find the
+			// element using predicate. This condition just make sure we only replace
+			// last element only if the new topCalories is bigger.
+			if totalCalories > tops[index-1] {
+				tops[index-1] = totalCalories
+			}
+			return
+		}
+
+		copy(tops[index+1:], tops[index:])
+		tops[index] = totalCalories
+	}
+
+	err := DoForElf(tokens, compute)
+
+	return tops, err
+}
+
+// Sum returns the sum of a slice of integers.
+func Sum[T constraints.Integer](nums []T) int {
+	var sum int = 0
+
+	for _, num := range nums {
+		sum += int(num)
+	}
+
+	return sum
+}
 
 func main() {
 	// open file
@@ -16,26 +93,11 @@ func main() {
 	}
 
 	tokens := strings.Split(string(input), "\n")
-	maxCalories := -1
-	currentCalories := 0
-
-	for _, token := range tokens {
-		if token == "" {
-			if currentCalories > maxCalories {
-				maxCalories = currentCalories
-			}
-			currentCalories = 0
-
-			continue
-		}
-
-		cal, err := strconv.Atoi(token)
-		if err != nil {
-			log.Fatalf("failed to parse calories: %s", err)
-		}
-
-		currentCalories += cal
+	top, err := Topk(tokens, 3)
+	if err != nil {
+		log.Fatalf("failed to find top 3: %s", err)
 	}
 
-	log.Printf("max calories: %d", maxCalories)
+	log.Printf("top k = %d", top)
+	log.Printf("max calories: %d", Sum(top))
 }
--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 Storing all of advent of code answers in this repository.
 
+## Getting started
+
+This repository uses [Earthly](https://earthly.dev) to build and run exercises.
+
+You can run the solutions for all of 2022 questions by doing:
+
+```
+cd 2022
+earthly +run
+```
+
+Or run a specific day solution by doing:
+
+```
+cd 2022/1
+earthly +run
+```
+
