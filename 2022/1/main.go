6a1d97b896739f5be968c6566244967ccba8c9ac
Lucas Clerisse <lucasclerisse@protonmail.com>
2022-12-01 21:39:13 +0800

    optimize solution

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -11,67 +11,43 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-type Elf struct {
-	// Bag contains the calories of each food
-	Bag []int
-}
-
-func (e Elf) TotalCalories() int {
-	cal := 0
-	for _, c := range e.Bag {
-		cal += c
-	}
-	return cal
-}
-
-// DoForElf executes the given function for every elf in the given input.
-func DoForElf(tokens []string, f func(Elf, int)) error {
-	bag := []int{}
-	idx := 0
-
-	for _, token := range tokens {
-		if token == "" {
-			f(Elf{bag}, idx)
-			// reset bag
-			bag = []int{}
-			idx += 1
-		} else {
-			cal, err := strconv.Atoi(token)
-			if err != nil {
-				log.Fatalf("failed to parse calories: %s", err)
-			}
-			bag = append(bag, cal)
-		}
-	}
-
-	return nil
-}
-
 // Topk returns the top k elves with the highest total calories.
 func Topk(tokens []string, k int) ([]int, error) {
 	tops := make([]int, k)
+	totalCalories := 0
 
-	compute := func(elf Elf, idx int) {
-		totalCalories := elf.TotalCalories()
-		index := sort.Search(k, func(index int) bool { return tops[index] <= totalCalories })
+	for _, token := range tokens {
+		if token == "" {
+			index := sort.Search(k, func(index int) bool { return tops[index] <= totalCalories })
 
-		if index == k {
-			// sort.Search returns the index of last element if it could not find the
-			// element using predicate. This condition just make sure we only replace
-			// last element only if the new topCalories is bigger.
-			if totalCalories > tops[index-1] {
-				tops[index-1] = totalCalories
+			if index >= k {
+				// sort.Search returns the index of last element if it could not find the
+				// element using predicate. This condition just make sure we only replace
+				// last element only if the new topCalories is bigger.
+				if totalCalories > tops[index-1] {
+					tops[index-1] = totalCalories
+				}
+				totalCalories = 0
+				continue
 			}
-			return
+
+			copy(tops[index+1:], tops[index:])
+			tops[index] = totalCalories
+
+			totalCalories = 0
+
+			continue
 		}
 
-		copy(tops[index+1:], tops[index:])
-		tops[index] = totalCalories
+		cal, err := strconv.Atoi(token)
+		if err != nil {
+			log.Fatalf("failed to parse calories: %s", err)
+		}
+
+		totalCalories += cal
 	}
 
-	err := DoForElf(tokens, compute)
-
-	return tops, err
+	return tops, nil
 }
 
 // Sum returns the sum of a slice of integers.
