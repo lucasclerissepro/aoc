90ae734ba0ea0f80074fbd4d5ff67d4aad22e62e
Lucas Clerisse <lucasclerisse@protonmail.com>
2022-12-01 20:45:04 +0800

    fix: benchmark

--- 2022/1/main_test.go
+++ 2022/1/main_test.go
@@ -13,17 +13,19 @@ func benchmarkSolution(k int, b *testing.B) {
 		b.Fatalf("failed to read input: %s", err)
 	}
 
-	tokens := strings.Split(string(input), "\n")
-	for n := 0; n < b.N; n++ {
-		_, err := Topk(tokens, 3)
-		if err != nil {
-			log.Fatalf("failed to find top 3: %s", err)
+	b.Run("solution", func(b *testing.B) {
+		tokens := strings.Split(string(input), "\n")
+		for n := 0; n < b.N; n++ {
+			tops, err := Topk(tokens, k)
+			if len(tops) != k || err != nil {
+				log.Fatalf("failed to find top 3: %s", err)
+			}
 		}
-	}
+	})
 }
 
-func BenchmarkSolutionTopk3(b *testing.B) { benchmarkSolution(3, b) }
-func BenchmarkSolutionTopk5(b *testing.B) { benchmarkSolution(5, b) }
+func BenchmarkSolutionTopk3(b *testing.B)  { benchmarkSolution(3, b) }
+func BenchmarkSolutionTopk5(b *testing.B)  { benchmarkSolution(5, b) }
 func BenchmarkSolutionTopk10(b *testing.B) { benchmarkSolution(10, b) }
 func BenchmarkSolutionTopk25(b *testing.B) { benchmarkSolution(25, b) }
 func BenchmarkSolutionTopk50(b *testing.B) { benchmarkSolution(50, b) }
